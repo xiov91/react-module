33afc7fb72e90001a5c7a05a8caeead9a56f2cee
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-09-06 14:02:18 -0400

    passing Nav tests

--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -1,54 +1,50 @@
-import React from 'react';
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
 
-    const categories = [
-        {
-          name: "commercial",
-          description:
-            "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-          name: "landscape",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-      ];
+  const  categories = [
+    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
+  ];
 
-    function categorySelected() {
-        console.log("hello")
-    }
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
 
   return (
-    <header>
-        <h2>
-            <a href="/">
-                <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => handleClick("About")}>
+              About me
             </a>
-        </h2>
-        <nav>
-            <ul className="flex-row">
-                <li className="mx-2">
-                    <a href="#about">
-                        About me
-                    </a>
-                </li>
-                <li>
-                    <span>Contact</span>
-                </li>
-                {categories.map((category) => (
-                    <li
-                        className="mx-1"
-                        key={category.name}
-                    >
-                        <span onClick={() => categorySelected(category.name)} >
-                            {category.name}
-                        </span>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+          </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick('Contact')}>
+              Contact
+            </span>
+          </li>
+          {
+            categories.map((category) => (
+              <li className="mx-1" key={category.name} >
+                <span onClick={() => { handleClick(category.name); }}>
+                 {capitalizeFirstLetter(category.name)}
+                </span>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
     </header>
   );
 }
