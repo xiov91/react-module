c41a8f95b002244031822906b5307ed340e64190
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-09-08 17:55:39 -0400

    SPA

--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -6,13 +6,15 @@ function Nav(props) {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
   } = props;
 
 
-  const handleClick = (item) => {
+  /* const handleClick = (item) => {
     console.log(item);
     return item;
-  };
+  }; */
 
   return (
     <header className="flex-row px-1">
@@ -24,25 +26,26 @@ function Nav(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick('Contact')}>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>
               Contact
             </span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name
+                currentCategory.name === category.name && !contactSelected && `navActive`
                 }`}
               key={category.name}
             >
               <span
                 onClick={() => {
                   setCurrentCategory(category);
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
